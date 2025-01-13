c47917f53932c8a6cacf4ef9c13462a62c7092a0
Zareef Intisar <zareefintiser@gmail.com>
2025-01-13 20:08:00 +0600

    again modified

--- Crash_Course/Chapter_2.py
+++ Crash_Course/Chapter_2.py
@@ -3,13 +3,44 @@
 #pg 19
 #exercise 2-1
 
-hi = "Hasina ran away"
-print(hi)
+# hi = "Hasina ran away"
+# print(hi)
+#
+# bye = "East or west barcelona the best"
+# print(bye)
+#
+# bye = "FC Barcelona :5  -  2: Real Madrid"
+# print(bye)
 
-bye = "East or west barcelona the best"
-print(bye)
 
-bye = "FC Barcelona :5  -  2: Real Madrid"
-print(bye)
+#pg 25
+#exercise 2-3
+# m = "Intisar"
+# print(f"Hello everyonee I'm {m}, I want to be a software engineer!")
+
+#exercise 2-4
+# i = "Zareef"
+# print(i.lower())
+# print(i.upper())
+# print(i.title())
+
+#exercis 2-5
+# print("“A person who never made a mistake never tried anything new.” -Albert Einstein")
+
+#exercise 2-6
+# famous_person = "Albert Einstein"
+# message = "“A person who never made a mistake never tried anything new.”"
+# print(f"{quote} -{name}")
+
+#exercise 2-7
+# z = "\t\t\t zareef \t\t\t\t"
+# print(z.lstrip().rstrip())
+
+
+
+
+
+
+
 
 
