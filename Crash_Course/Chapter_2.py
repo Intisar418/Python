ed1712c615925687ae350bed9df03915c61a77fb
Zareef Intisar <zareefintiser@gmail.com>
2025-01-16 21:55:03 +0600

    modified all

--- Crash_Course/Chapter_2.py
+++ Crash_Course/Chapter_2.py
@@ -3,57 +3,57 @@
 #pg 19
 #exercise 2-1
 
-# hi = "Hasina ran away"
-# print(hi)
-#
-# bye = "East or west barcelona the best"
-# print(bye)
-#
-# bye = "FC Barcelona :5  -  2: Real Madrid"
-# print(bye)
-#
+hi = "Hasina ran away"
+print(hi)
+
+bye = "East or west barcelona the best"
+print(bye)
+
+bye = "FC Barcelona :5  -  2: Real Madrid"
+print(bye)
+
 
 #pg 25
 #exercise 2-3
-# m = "Intisar"
-# print(f"Hello everyonee I'm {m}, I want to be a software engineer!")
+m = "Intisar"
+print(f"Hello everyonee I'm {m}, I want to be a software engineer!")
 
 #exercise 2-4
-# i = "Zareef"
-# print(i.lower())
-# print(i.upper())
-# print(i.title())
+i = "Zareef"
+print(i.lower())
+print(i.upper())
+print(i.title())
 
 #exercis 2-5
-# print("“A person who never made a mistake never tried anything new.” -Albert Einstein")
+print("“A person who never made a mistake never tried anything new.” -Albert Einstein")
 
-#exercise 2-6
-# famous_person = "Albert Einstein"
-# message = "“A person who never made a mistake never tried anything new.”"
-# print(f"{famous_person} -{message}")
+# exercise 2-6
+famous_person = "Albert Einstein"
+message = "“A person who never made a mistake never tried anything new.”"
+print(f"{famous_person} -{message}")
 
 #exercise 2-7
-# z = "\t\t\t zareef \t\t\t\t"
-# print(z.lstrip().rstrip())
+z = "\t\t\t zareef \t\t\t\t"
+print(z.lstrip().rstrip())
 
 #pg 29
 #exercise 2-8
-# print(2+6)
-# print(12-4)
-# print(4*2)
-# print(64/8)
+print(2+6)
+print(12-4)
+print(4*2)
+print(64/8)
 
 #exercise 2-9
-# m = 10
-# print(f"My favourite number is {m}")
+m = 10
+print(f"My favourite number is {m}")
 
 #pg 30
 #exercise 2-10
                 # Age checker #
-# m = int(input("Enter your birth year: "))
-# i = int(input("Enter the current year: "))
-# if i<m :
-#     print("Please enter the year correctly")
-# else:
-#     print(f"Your age is {i-m}")
+m = int(input("Enter your birth year: "))
+i = int(input("Enter the current year: "))
+if i<m :
+    print("Please enter the year correctly")
+else:
+    print(f"Your age is {i-m}")
 
--- Crash_Course/Chapter_3.py
+++ Crash_Course/Chapter_3.py
@@ -1,46 +1,46 @@
                                             #### All programs from chapter 3 ####
 #pg 36
 #exercise 3-1
-# names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
-# for i in names:
-#     print(i)
+names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
+for i in names:
+    print(i)
 
 #exercise 3-2
-# names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
-# for i in names:
-#     print(f"Hey! How are you {i}?")
+names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
+for i in names:
+    print(f"Hey! How are you {i}?")
 
 
 #exercise 3-3
-# dream_items = ['Rolls - Royce', 'Lamborghini', 'Gaming PC']
-# for m in dream_items:
-#     print(f"One day i will have owned a {m}")
+dream_items = ['Rolls - Royce', 'Lamborghini', 'Gaming PC']
+for m in dream_items:
+    print(f"One day i will have owned a {m}")
 
 #pg 42
-# exercise 3-4
-# guest = ['Kafi', 'Ayan', 'Intisar']
-# for i in guest:
-#     print(f"Dear {i}, you're requested to join my dinner party")
+exercise 3-4
+guest = ['Kafi', 'Ayan', 'Intisar']
+for i in guest:
+    print(f"Dear {i}, you're requested to join my dinner party")
 
 
 #exercise 3-5
-# guest = ['Kafi', 'Ayan', 'Intisar']
-# print(guest[2], ", just I have heard that you can't join my dinner party")
-# guest.remove('Intisar')
-# guest.append('Zareef')
-# print('\n')
-# for i in guest:
-#     print(f"Dear {i}, you're requested to join my dinner party")
+guest = ['Kafi', 'Ayan', 'Intisar']
+print(guest[2], ", just I have heard that you can't join my dinner party")
+guest.remove('Intisar')
+guest.append('Zareef')
+print('\n')
+for i in guest:
+    print(f"Dear {i}, you're requested to join my dinner party")
 
 #exercise 3-6
-# guest = ['Kafi', 'Ayan', 'Zareef']
-# print( "I have found a bigger dinner table, now I'm going to invite more people ")
-# print("\t")
-# guest.insert(3 ,'Sowad')
-# guest.insert(4,'Ahnaf')
-# guest.append('Tawfiq')
-# for m in guest:
-#     print(f"\t {m} you're requested to join my dinner party")
+guest = ['Kafi', 'Ayan', 'Zareef']
+print( "I have found a bigger dinner table, now I'm going to invite more people ")
+print("\t")
+guest.insert(3 ,'Sowad')
+guest.insert(4,'Ahnaf')
+guest.append('Tawfiq')
+for m in guest:
+    print(f"\t {m} you're requested to join my dinner party")
 
 #exercise 3-7
 
@@ -48,13 +48,17 @@
 
 #pg 46
 #exercise 3-8
-# locations = ['Sylhet', 'Cox\'s Bazar', 'Rosario', 'Gulistan', 'Russia' ]
-# print(sorted(locations))
-# print(locations)
-
-
-
-
-
-
+locations = ['Sylhet', 'Cox\'s Bazar', 'Rosario', 'Gulistan', 'Russia' ]
+print(sorted(locations))
+print(locations)
+print(sorted(locations, reverse = True))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort()
+print(locations)
 
