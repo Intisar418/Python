3612c752fab86d7fefe4a51edd801dd206d0d854
Zareef Intisar <zareefintiser@gmail.com>
2025-01-17 22:43:50 +0600

    addd a new chapter

--- Crash_Course/Chapter_4.py
+++ Crash_Course/Chapter_4.py
@@ -1,10 +1,48 @@
-#                                             #### All programs from chapter 4 ####
-
+                                            #### All programs from chapter 4 ####
+#pg 56
+#exercise 4-1
 pizza = ["BBQ Chicken Pizza", "Four Cheese Pizza", "Hawaiian Pizza"]
 for p in pizza:
     print(p)
 for m in pizza:
     print(f"I like {m} ")
+print("""Pizza is my favourite food. Pizaa isn't good for health. That's why I
+don't eat pizza too much. My favourite pizza types are given below""")
+for i in pizza:
+    print(i)
+
+#exercise 4-2
+animals = ['Dog', 'Cat', 'Bird']
+for k in animals:
+    print(k)
+for a in animals:
+    print(f"A {a} is a very good animal.")
+
+#exercise 4-3
+for i in range(1, 21):
+    print(i)
+
+#exercise 4-4
+m = list(range(1, 1000001))
+for i in m:
+    print(i)
+
+#exercise 4-5
+hi = list(range(1, 1000001))
+print(min(hi))
+print(max(hi))
+print(sum(hi))
+
+#exercise 4-6
+
+
+
+
+
+
+
+
+
 
 
 
