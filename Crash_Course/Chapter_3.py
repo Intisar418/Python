81299560e59a7ecb2628f45605038d5142ce2cb6
Zareef Intisar <zareefintiser@gmail.com>
2025-01-17 20:40:39 +0600

    chapter 3 4 modified

--- Crash_Course/Chapter_3.py
+++ Crash_Course/Chapter_3.py
@@ -1,64 +1,83 @@
-                                            #### All programs from chapter 3 ####
-#pg 36
-#exercise 3-1
-names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
-for i in names:
-    print(i)
+#                                             #### All programs from chapter 3 ####
+# #pg 36
+# #exercise 3-1
+# names = ['Kafi', 'Tawfiq', 'vilRadif', 'Ayan']
+# for i in names:
+#     print(i)
+#
+# #exercise 3-2
+# names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
+# for i in names:
+#     print(f"Hey! How are you {i}?")
+#
+#
+# #exercise 3-3
+# dream_items = ['Rolls - Royce', 'Lamborghini', 'Gaming PC']
+# for m in dream_items:
+#     print(f"One day i will have owned a {m}")
+#
+# #pg 42
+# # exercise 3-4
+# guest = ['Kafi', 'Ayan', 'Intisar']
+# for i in guest:
+#     print(f"Dear {i}, you're requested to join my dinner party")
+#
+#
+# #exercise 3-5
+# guest = ['Kafi', 'Ayan', 'Intisar']
+# print(guest[2], ", just I have heard that you can't join my dinner party")
+# guest.remove('Intisar')
+# guest.append('Zareef')
+# print('\n')
+# for i in guest:
+#     print(f"Dear {i}, you're requested to join my dinner party")
+#
+# #exercise 3-6
+# guest = ['Kafi', 'Ayan', 'Zareef']
+# print( "I have found a bigger dinner table, now I'm going to invite more people ")
+# print("\t")
+# guest.insert(3 ,'Sowad')
+# guest.insert(4,'Ahnaf')
+# guest.append('Tawfiq')
+# for m in guest:
+#     print(f"\t {m} you're requested to join my dinner party")
+#
+# #exercise 3-7
+#
+#
+#
+# #pg 46
+# #exercise 3-8
+# locations = ['Sylhet', 'Cox\'s Bazar', 'Rosario', 'Gulistan', 'Russia' ]
+# print(sorted(locations))
+# print(locations)
+# print(sorted(locations, reverse = True))
+# print(locations)
+# locations.reverse()
+# print(locations)
+# locations.reverse()
+# print(locations)
+# locations.sort()
+# print(locations)
+# locations.sort()
+# print(locations)
+#
+# #exercise 3-10
+# countries = ['Bangladesh', 'Argentina', 'Palestine', 'Russia', 'Qatar']
+# countries.pop()
+# countries.append('pakistan')
+# countries.insert(2, 'Switzerland')
+# countries.append('India')
+# countries.sort()
+# countries.reverse()
+# countries.pop(4)
+# countries.sort()
+# print(countries)
 
-#exercise 3-2
-names = ['Kafi', 'Tawfiq', 'Radif', 'Ayan']
-for i in names:
-    print(f"Hey! How are you {i}?")
-
-
-#exercise 3-3
-dream_items = ['Rolls - Royce', 'Lamborghini', 'Gaming PC']
-for m in dream_items:
-    print(f"One day i will have owned a {m}")
-
-#pg 42
-exercise 3-4
-guest = ['Kafi', 'Ayan', 'Intisar']
-for i in guest:
-    print(f"Dear {i}, you're requested to join my dinner party")
-
-
-#exercise 3-5
-guest = ['Kafi', 'Ayan', 'Intisar']
-print(guest[2], ", just I have heard that you can't join my dinner party")
-guest.remove('Intisar')
-guest.append('Zareef')
-print('\n')
-for i in guest:
-    print(f"Dear {i}, you're requested to join my dinner party")
-
-#exercise 3-6
-guest = ['Kafi', 'Ayan', 'Zareef']
-print( "I have found a bigger dinner table, now I'm going to invite more people ")
-print("\t")
-guest.insert(3 ,'Sowad')
-guest.insert(4,'Ahnaf')
-guest.append('Tawfiq')
-for m in guest:
-    print(f"\t {m} you're requested to join my dinner party")
-
-#exercise 3-7
 
 
 
-#pg 46
-#exercise 3-8
-locations = ['Sylhet', 'Cox\'s Bazar', 'Rosario', 'Gulistan', 'Russia' ]
-print(sorted(locations))
-print(locations)
-print(sorted(locations, reverse = True))
-print(locations)
-locations.reverse()
-print(locations)
-locations.reverse()
-print(locations)
-locations.sort()
-print(locations)
-locations.sort()
-print(locations)
+
+
+
 
---
+++ Crash_Course/Chapter_4.py
@@ -0,0 +1,16 @@
+#                                             #### All programs from chapter 4 ####
+
+pizza = ["BBQ Chicken Pizza", "Four Cheese Pizza", "Hawaiian Pizza"]
+for p in pizza:
+    print(p)
+for m in pizza:
+    print(f"I like {m} ")
+
+
+
+
+
+
+
+
+
