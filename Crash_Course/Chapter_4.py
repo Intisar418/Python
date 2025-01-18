def05b3dc4ed6b779fe4ab74032b2eae5f9cb077
Zareef Intisar <zareefintiser@gmail.com>
2025-01-18 20:59:32 +0600

    hi

--- Crash_Course/Chapter_4.py
+++ Crash_Course/Chapter_4.py
@@ -1,6 +1,6 @@
                                             #### All programs from chapter 4 ####
-#pg 56
-#exercise 4-1
+# #pg 56
+# exercise 4-1
 pizza = ["BBQ Chicken Pizza", "Four Cheese Pizza", "Hawaiian Pizza"]
 for p in pizza:
     print(p)
@@ -18,35 +18,42 @@ for k in animals:
 for a in animals:
     print(f"A {a} is a very good animal.")
 
+#pg 60
 #exercise 4-3
 for i in range(1, 21):
     print(i)
 
-#exercise 4-4
+# #exercise 4-4
 m = list(range(1, 1000001))
 for i in m:
     print(i)
 
-#exercise 4-5
+# #exercise 4-5
 hi = list(range(1, 1000001))
 print(min(hi))
 print(max(hi))
 print(sum(hi))
 
-#exercise 4-6
+# #exercise 4-6
+m = list(range(1, 21, 2))
+for i in m:
+    print(i)
 
+#exercise 4-7
+k = list(range(3, 31, 3))
+for i in k:
+    print(i)
 
+#exercise 4-8
+c = list(range(1, 11, 1))
+z = []
+for i in c:
+    k = i**2
+    z.append(k)
+print(z)
 
-
-
-
-
-
-
-
-
-
-
+#pg 65
+#exercise 4-10
 
 
 
