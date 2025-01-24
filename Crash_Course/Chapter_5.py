e168c06c2cbe6381538443a20415680e0b7174ea
Zareef Intisar <zareefintiser@gmail.com>
2025-01-24 17:29:07 +0600

    many days later

---
+++ Crash_Course/Chapter_5.py
@@ -0,0 +1,16 @@
+                                           #### All programs from chapter 5 ####
+# #pg 84
+# # exercise 5-3
+alien_color = 'green'
+if alien_color == ' green':
+    print("+5 points")
+    
+
+
+
+
+
+
+
+
+
