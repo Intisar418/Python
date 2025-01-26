496ac637f1f514c42dad56ca2990679ca0223aca
Zareef Intisar <zareefintiser@gmail.com>
2025-01-26 19:41:28 +0600

    functions

---
+++ Crash_Course/Chapter_8.py
@@ -0,0 +1,13 @@
+                                                #### All programs from chapter 8 ####
+def zareef():
+    print("Hello World!")
+zareef()
+
+def username(hi):
+    print(f"Hello, {hi.title()}")
+username('zareef')
+
+def display_message(lm10):
+    print(f"{lm10}")
+display_message('Hello everyone I\'m learning functions')
+
