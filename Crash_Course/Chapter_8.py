b977f2fc5c0bbf80ef9d3e5d3c7b9c21dbc5f4e6
Zareef Intisar <zareefintiser@gmail.com>
2025-01-27 20:50:17 +0600

    2024 dhanmondir apa no more

--- Crash_Course/Chapter_8.py
+++ Crash_Course/Chapter_8.py
@@ -1,13 +1,47 @@
-                                                #### All programs from chapter 8 ####
-def zareef():
-    print("Hello World!")
-zareef()
+#                                                 #### All programs from chapter 8 ####
+# def zareef():
+#     print("Hello World!")
+# zareef()
+#
+# def username(hi):
+#     print(f"Hello, {hi.title()}")
+# username('zareef')
+#
+# def display_message(lm10):
+#     print(f"{lm10}")
+# display_message('Hello everyone I\'m learning functions')
 
-def username(hi):
-    print(f"Hello, {hi.title()}")
-username('zareef')
+# def favourite_book(title):
+#     print(f"One of my favourite book is {title}")
+# favourite_book('Alice in Wonderland')
 
-def display_message(lm10):
-    print(f"{lm10}")
-display_message('Hello everyone I\'m learning functions')
+# def my_pet(animal_type,animal_name):
+#     print(f"I have a {animal_type} "
+#           f"\nMy {animal_type}'s name is {animal_name}")
+# my_pet('Cat','Murgi')
+
+# def animal(x, y):
+#     print(f"I have a {x} \nHis name is {y}")
+# animal(x="Dog",y="Hasina")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
