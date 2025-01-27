1c969235f4a07ee16adf94d1895c97e88ba5982c
Zareef Intisar <zareefintiser@gmail.com>
2025-01-27 20:55:42 +0600

    2024 Dhanmondir apa no more

--- Crash_Course/Chapter_8.py
+++ Crash_Course/Chapter_8.py
@@ -1,28 +1,28 @@
-#                                                 #### All programs from chapter 8 ####
-# def zareef():
-#     print("Hello World!")
-# zareef()
-#
-# def username(hi):
-#     print(f"Hello, {hi.title()}")
-# username('zareef')
-#
-# def display_message(lm10):
-#     print(f"{lm10}")
-# display_message('Hello everyone I\'m learning functions')
+                                                #### All programs from chapter 8 ####
+def zareef():
+    print("Hello World!")
+zareef()
 
-# def favourite_book(title):
-#     print(f"One of my favourite book is {title}")
-# favourite_book('Alice in Wonderland')
+def username(hi):
+    print(f"Hello, {hi.title()}")
+username('zareef')
 
-# def my_pet(animal_type,animal_name):
-#     print(f"I have a {animal_type} "
-#           f"\nMy {animal_type}'s name is {animal_name}")
-# my_pet('Cat','Murgi')
+def display_message(lm10):
+    print(f"{lm10}")
+display_message('Hello everyone I\'m learning functions')
 
-# def animal(x, y):
-#     print(f"I have a {x} \nHis name is {y}")
-# animal(x="Dog",y="Hasina")
+def favourite_book(title):
+    print(f"One of my favourite book is {title}")
+favourite_book('Alice in Wonderland')
+
+def my_pet(animal_type,animal_name):
+    print(f"I have a {animal_type} "
+          f"\nMy {animal_type}'s name is {animal_name}")
+my_pet('Cat','Murgi')
+
+def animal(x, y):
+    print(f"I have a {x} \nHis name is {y}")
+animal(x="Dog",y="Hasina")
 
 
 
