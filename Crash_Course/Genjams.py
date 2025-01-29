7143132387b2962059f90ca30e07f0176f5a1da8
Zareef Intisar <zareefintiser@gmail.com>
2025-01-29 19:50:05 +0600

    Hello World!

--- Crash_Course/Genjams.py
+++ Crash_Course/Genjams.py
@@ -1,36 +1,40 @@
-#
-# #### solution 1 ####
-# num = int(input("Enter a number: "))
-# is_prime = True
-# if num <= 1:
-#     is_prime = False
-# else:
-#     for i in range(2, int(num ** 0.5) + 1):
-#         if num % i == 0:
-#             is_prime = False
-#             break
-# if is_prime:
-#     print(f"{num} is a prime number.")
-# else:
-#     print(f"{num} is not a prime number.")
-#
-# #### solution 2 ####
-# num = int(input("Enter a number: "))
-# if num <= 1:
-#     print(f"{num} is not a prime number.")
-# else:
-#     is_prime = True
-#     for i in range(2, num):
-#         if num % i == 0:
-#             is_prime = False
-#             break
-#     if is_prime:
-#         print(f"{num} is a prime number.")
-#     else:
-#         print(f"{num} is not a prime number.")
-#
+
+#### solution 1 ####
+num = int(input("Enter a number: "))
+is_prime = True
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+if is_prime:
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
+
+#### solution 2 ####
+num = int(input("Enter a number: "))
+if num <= 1:
+    print(f"{num} is not a prime number.")
+else:
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
+
+
 
 i = int(input("Enter a number: "))
 print(i/(i*i**i))
 
-
+m = input("Enter a number: ")
+for i in range(1, m+1):
+    i/(i*i)
+print(i)
