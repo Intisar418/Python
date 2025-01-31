3c229356c89029df10ef1df09639364cca44a3d8
Zareef Intisar <zareefintiser@gmail.com>
2025-01-31 19:30:05 +0600

    Hello there

--- Crash_Course/Chapter_5.py
+++ Crash_Course/Chapter_5.py
@@ -1,43 +1,69 @@
-                                           ### All programs from chapter 5 ####
-#pg 84
-# exercise 5-3
-alien_color = 'green'
-if alien_color == ' green':
-    print("+5 points")
+#                                            ### All programs from chapter 5 ####
+# #pg 84
+# # exercise 5-3
+# alien_color = 'green'
+# if alien_color == ' green':
+#     print("+5 points")
+#
+# # #exercise 5-4
+# alien_color = input("enter alien color(color must be green,red or yellow): ")
+# if alien_color.lower() == 'green':
+#     print("+5 points")
+# else:
+#     print('+10 points')
+#
+# #pg 85
+# #exercise 5-5
+# alien_color = input("enter alien color N:B: COLOR MUST BE RED, GREEN, OR YELLOW: ")
+# m = alien_color.lower()
+# if m == 'green':
+#     print("+5 points")
+# elif m == 'yellow':
+#     print("+10 points")
+# elif m == 'red':
+#     print("+15 points")
+# else:
+#     print("Please enter the name of color correctly!")
+#
+# # #exercise 5-6
+# while True:
+#     i = int(input("Enter your age: "))
+#     if i <2:
+#         print("You are a baby")
+#     elif i >= 2 and i < 4:
+#         print("You are a toddler")
+#     elif i >= 4 and i <13:
+#         print("You are a kid")
+#     elif i >= 13 and i < 20:
+#         print("You are a teen")
+#     elif i >= 20 and i < 65:
+#         print("You are an adult")
+#     else:
+#         print("You are an elder")
 
-# #exercise 5-4
-alien_color = input("enter alien color(color must be green,red or yellow): ")
-if alien_color.lower() == 'green':
-    print("+5 points")
-else:
-    print('+10 points')
+# #exercise 5-7
+# favourite_fruits = ['Apple', 'Lychee', 'Strawberry']
+# if 'Mango' in favourite_fruits:
+#     print("You really like Mangoes!")
+# if 'Lychee' in favourite_fruits:
+#     print("You really like Lychees!")
+# if 'Strawberry' in favourite_fruits:
+#     print("Yoy really like Strawberries!")
+# if 'Banana' in favourite_fruits:
+#     print("You really like banana")
+# if 'Apple' in favourite_fruits:
+#     print("You are a Newton!")
+# #pg-89
+# #exercise 5-8
+# hello_users = ['admin', 'Zareef', 'Intisar', 'Ovy', 'Ocean']
+# for i in hello_users:
+#     if i == 'Ovy':
+#         print(f"Hello {i} vaiya, thank you for logging in again")
+#     elif i == 'admin':
+#         print(f"Hello {i}, would you like to see a status report?")
+#     else:
+#         print(f"Hello {i}, thank you for logging again")
 
-#pg 85
-#exercise 5-5
-alien_color = input("enter alien color N:B: COLOR MUST BE RED, GREEN, OR YELLOW: ")
-m = alien_color.lower()
-if m == 'green':
-    print("+5 points")
-elif m == 'yellow':
-    print("+10 points")
-elif m == 'red':
-    print("+15 points")
-else:
-    print("Please enter the name of color correctly!")
+# #exercise 5-9
 
-# #exercise 5-6
-while True:
-    i = int(input("Enter your age: "))
-    if i <2:
-        print("You are a baby")
-    elif i >= 2 and i < 4:
-        print("You are a toddler")
-    elif i >= 4 and i <13:
-        print("You are a kid")
-    elif i >= 13 and i < 20:
-        print("You are a teen")
-    elif i >= 20 and i < 65:
-        print("You are an adult")
-    else:
-        print("You are an elder")
 
