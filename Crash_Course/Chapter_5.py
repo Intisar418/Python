7a3ebd0c74cc694cbcc4f9cea2f698a0be5f393e
Zareef Intisar <zareefintiser@gmail.com>
2025-02-02 16:58:56 +0600

    bye

--- Crash_Course/Chapter_5.py
+++ Crash_Course/Chapter_5.py
@@ -1,84 +1,90 @@
-                                           ### All programs from chapter 5 ####
-#pg 84
-# exercise 5-3
-alien_color = 'green'
-if alien_color == ' green':
-    print("+5 points")
+#                                            ### All programs from chapter 5 ####
+# #pg 84
+# # exercise 5-3
+# alien_color = 'green'
+# if alien_color == ' green':
+#     print("+5 points")
+#
+# # #exercise 5-4
+# alien_color = input("enter alien color(color must be green,red or yellow): ")
+# if alien_color.lower() == 'green':
+#     print("+5 points")
+# else:
+#     print('+10 points')
+#
+# #pg 85
+# #exercise 5-5
+# alien_color = input("enter alien color N:B: COLOR MUST BE RED, GREEN, OR YELLOW: ")
+# m = alien_color.lower()
+# if m == 'green':
+#     print("+5 points")
+# elif m == 'yellow':
+#     print("+10 points")
+# elif m == 'red':
+#     print("+15 points")
+# else:
+#     print("Please enter the name of color correctly!")
+#
+# # #exercise 5-6
+# while True:
+#     i = int(input("Enter your age: "))
+#     if i <2:
+#         print("You are a baby")
+#     elif i >= 2 and i < 4:
+#         print("You are a toddler")
+#     elif i >= 4 and i <13:
+#         print("You are a kid")
+#     elif i >= 13 and i < 20:
+#         print("You are a teen")
+#     elif i >= 20 and i < 65:
+#         print("You are an adult")
+#     else:
+#         print("You are an elder")
+#
+# #exercise 5-7
+# favourite_fruits = ['Apple', 'Lychee', 'Strawberry']
+# if 'Mango' in favourite_fruits:
+#     print("You really like Mangoes!")
+# if 'Lychee' in favourite_fruits:
+#     print("You really like Lychees!")
+# if 'Strawberry' in favourite_fruits:
+#     print("Yoy really like Strawberries!")
+# if 'Banana' in favourite_fruits:
+#     print("You really like banana")
+# if 'Apple' in favourite_fruits:
+#     print("You are a Newton!")
+# #pg-89
+# #exercise 5-8 & 5-9
+# hello_users = ['admin', 'Zareef', 'Intisar', 'Ovy', 'Ocean']
+# for i in hello_users:
+#     if i == 'Ovy':
+#         print(f"Hello {i} vaiya, thank you for logging in again")
+#     elif i == 'admin':
+#         print(f"Hello {i}, would you like to see a status report?")
+#     else:
+#         print(f"Hello {i}, thank you for logging in again")
+#
+# hello_users.pop()
+# hello_users.pop()
+# hello_users.pop()
+# hello_users.pop()
+# hello_users.pop()
+# if not hello_users:
+#     print("We need to find some users!")
 
-# #exercise 5-4
-alien_color = input("enter alien color(color must be green,red or yellow): ")
-if alien_color.lower() == 'green':
-    print("+5 points")
-else:
-    print('+10 points')
-
-#pg 85
-#exercise 5-5
-alien_color = input("enter alien color N:B: COLOR MUST BE RED, GREEN, OR YELLOW: ")
-m = alien_color.lower()
-if m == 'green':
-    print("+5 points")
-elif m == 'yellow':
-    print("+10 points")
-elif m == 'red':
-    print("+15 points")
-else:
-    print("Please enter the name of color correctly!")
-
-# #exercise 5-6
-while True:
-    i = int(input("Enter your age: "))
-    if i <2:
-        print("You are a baby")
-    elif i >= 2 and i < 4:
-        print("You are a toddler")
-    elif i >= 4 and i <13:
-        print("You are a kid")
-    elif i >= 13 and i < 20:
-        print("You are a teen")
-    elif i >= 20 and i < 65:
-        print("You are an adult")
-    else:
-        print("You are an elder")
-
-#exercise 5-7
-favourite_fruits = ['Apple', 'Lychee', 'Strawberry']
-if 'Mango' in favourite_fruits:
-    print("You really like Mangoes!")
-if 'Lychee' in favourite_fruits:
-    print("You really like Lychees!")
-if 'Strawberry' in favourite_fruits:
-    print("Yoy really like Strawberries!")
-if 'Banana' in favourite_fruits:
-    print("You really like banana")
-if 'Apple' in favourite_fruits:
-    print("You are a Newton!")
-#pg-89
-#exercise 5-8 & 5-9
-hello_users = ['admin', 'Zareef', 'Intisar', 'Ovy', 'Ocean']
-for i in hello_users:
-    if i == 'Ovy':
-        print(f"Hello {i} vaiya, thank you for logging in again")
-    elif i == 'admin':
-        print(f"Hello {i}, would you like to see a status report?")
-    else:
-        print(f"Hello {i}, thank you for logging again")
-
-hello_users.pop()
-hello_users.pop()
-hello_users.pop()
-hello_users.pop()
-hello_users.pop()
-if not hello_users:
-    print("We need to find some users!")
-
-#exercise 5-10
-current_users = ['Zareef', 'Intisar', 'Ayan', 'Kafi', 'Tawfiq']
-new_users = ['Xenon', 'Radif', 'Zuhana','Isam']
+# # #exercise 5-11
+# Ordinal_Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in Ordinal_Numbers:
+#     if i == 1:
+#         print(f"{i}st")
+#     elif i == 2:
+#         print(f"{i}nd")
+#     elif i == 3:
+#         print(f"{i}rd")
+#     else:
+#         print(f"{i}th")
 
 
 
 
 
-
-print("Hello World!")
