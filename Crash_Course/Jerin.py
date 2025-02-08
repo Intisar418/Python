d2a3fbe0016b4d56f195ed4d5a8811f31c684811
Zareef Intisar <zareefintiser@gmail.com>
2025-02-08 21:29:49 +0600

    tuni re maro

--- Crash_Course/Jerin.py
+++ Crash_Course/Jerin.py
@@ -1,6 +1,8 @@
                                           #### All programs of Jerin ####
 
 
+def find_minimum(numbers):
+    return min(numbers)
 
-
-
+numbers_list = [9,8,97,57,10,7,1089,71,24]
+print(find_minimum(numbers_list))
