c4af22bf2fa05d2c918cc0d48b1044f83936c984
Zareef Intisar <zareefintiser@gmail.com>
2025-02-11 23:11:37 +0600

    2 hours

--- Crash_Course/Chapter_6.py
+++ Crash_Course/Chapter_6.py
@@ -1,41 +1,57 @@
-                                #### All programs of chapter 6 ####
-# #pg 99
-# #exercise 6-1
-person = {
-    'first_name': input("Enter your first name: "),
-    'last_name': input("Enter your last name: "),
-    'age': int(input("Enter your age: ")),
-    'city': input("Enter the name of your city: "),
-}
-print("\n\n")
-print(f"First name: {person['first_name']}")
-print(f"Last name: {person['last_name']}")
-print(f"Age: {person['age']}")
-print(f"City: {person['city']} ")
+#                                 #### All programs of chapter 6 ####
+# # #pg 99
+# # #exercise 6-1
+# person = {
+#     'first_name': input("Enter your first name: "),
+#     'last_name': input("Enter your last name: "),
+#     'age': int(input("Enter your age: ")),
+#     'city': input("Enter the name of your city: "),
+# }
+# print("\n\n")
+# print(f"First name: {person['first_name']}")
+# print(f"Last name: {person['last_name']}")
+# print(f"Age: {person['age']}")
+# print(f"City: {person['city']} ")
+#
+#
+# # #exercise 6-2
+# favourite_number = {'Messi_1': 10,
+#                     'Messi_2': 19,
+#                     'Messi_3': 18,
+#                     'Messi_4': 30,
+#                     'Penaldo': 7
+# }
+#
+# for name, number in favourite_number.items():
+#     print(f"Favourite number of {name} is {number}")
+#
+# # pg 105
+# # #exercise 6-5
+# rivers = {'Nile': 'Egypt', 'Amazon RIver': 'Brazil',
+#           'Mississippi River': 'USA'}
+# for river, country in rivers.items():
+#     print(f"The {river} runs through {country}")
+# for m in rivers:
+#     print(m)
+# for i in rivers.values():
+#     print(i)
 
+# #pg 112
+# #exercise  6-7
+person1 = {'Name': 'Zareef Intisar',
+           'Age': 14,
+           'DOB': '20th April 2010',
+           'Net Worth': '$20B'
+           }
+person2 = {'Name': 'Elon Musk',
+           'Age': 55,
+           'DOB': 'Not found',
+           'Net Worth': '$230B'
+           }
 
-# #exercise 6-2
-favourite_number = {'Messi_1': 10,
-                    'Messi_2': 19,
-                    'Messi_3': 18,
-                    'Messi_4': 30,
-                    'Penaldo': 7
-}
-
-for name, number in favourite_number.items():
-    print(f"Favourite number of {name} is {number}")
-
-# pg 105
-# #exercise 6-5
-rivers = {'Nile': 'Egypt', 'Amazon RIver': 'Brazil',
-          'Mississippi River': 'USA'}
-for river, country in rivers.items():
-    print(f"The {river} runs through {country}")
-
-
-
-
-
+lst = [person1,person2]
+for i in lst:
+    print(i)
 
 
 
